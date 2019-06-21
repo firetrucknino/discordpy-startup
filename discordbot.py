613b16582be8da530aa1503d2a8bf4d5605d59d0
firetrucknino <52083739+firetrucknino@users.noreply.github.com>
2019-06-22 04:13:54 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,16 +1,21 @@
+#coding:UTF-8
 import discord
 from discord.ext import tasks
+from datetime import datetime 
 
-TOKEN = "NTkxNjg3MzIyNTg0NzQzOTM2.XQ0chA.DFt5eQ2TUFuWloCnwhllfxaKzaA" #トークン
-CHANNEL_ID = nortification#3907 #チャンネルID
+TOKEN = "**********" #トークン
+CHANNEL_ID = ********** #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
 # 60秒に一回ループ
-@tasks.loop(seconds=5)
+@tasks.loop(seconds=60)
 async def loop():
-    channel = client.get_channel(CHANNEL_ID)
-    await channel.send('時間だよ')  
+    # 現在の時刻
+    now = datetime.now().strftime('%H:%M')
+    if now == '07:00':
+        channel = client.get_channel(CHANNEL_ID)
+        await channel.send('おはよう')  
 
 #ループ処理実行
 loop.start()
