a3a924d5199ecc85efdba2cc7cd08d6493b87f6b
firetrucknino <52083739+firetrucknino@users.noreply.github.com>
2019-06-22 04:02:45 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -1,8 +1,8 @@
 import discord
 from discord.ext import tasks
 
-TOKEN = "**********" #トークン
-CHANNEL_ID = ********** #チャンネルID
+TOKEN = "NTkxNjg3MzIyNTg0NzQzOTM2.XQ0chA.DFt5eQ2TUFuWloCnwhllfxaKzaA" #トークン
+CHANNEL_ID = nortification#3907 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
