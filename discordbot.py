ad734ae59b0cacc3fb6f8260bf4f597f0807cb45
firetrucknino <52083739+firetrucknino@users.noreply.github.com>
2019-06-22 20:38:56 +0900

    Update discordbot.py

--- discordbot.py
+++ discordbot.py
@@ -3,8 +3,8 @@ import discord
 from discord.ext import tasks
 from datetime import datetime 
 
-TOKEN = "**********" #トークン
-CHANNEL_ID = ********** #チャンネルID
+TOKEN = "NTkxNjg3MzIyNTg0NzQzOTM2.XQ4TDg.Hl_Rui4m1TJ9bjvKCRc_GpSV7wk" #トークン
+CHANNEL_ID = notification#3907 #チャンネルID
 # 接続に必要なオブジェクトを生成
 client = discord.Client()
 
